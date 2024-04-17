7d64cd047b9d0525d2d3c20788a7c1f11bc50c59
chadelofson <chad.elofson@gmail.com>
2024-04-17 12:56:02 -0700

    Adding code up to Lecture 65

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/alexedwards/scs/v2"
-	"github.com/chadelofson/bookings/pkg/config"
-	handlers "github.com/chadelofson/bookings/pkg/handlers"
-	"github.com/chadelofson/bookings/pkg/render"
+	"github.com/chadelofson/bookings/internal/config"
+	"github.com/chadelofson/bookings/internal/handlers"
+	"github.com/chadelofson/bookings/internal/render"
 	"log"
 	"net/http"
 	"time"
--- internal/render/render.go
+++ internal/render/render.go
@@ -2,8 +2,9 @@ package render
 
 import (
 	"bytes"
-	"github.com/chadelofson/bookings/pkg/config"
-	"github.com/chadelofson/bookings/pkg/models"
+	"github.com/chadelofson/bookings/internal/config"
+	"github.com/chadelofson/bookings/internal/models"
+	"github.com/justinas/nosurf"
 	"html/template"
 	"log"
 	"net/http"
@@ -17,12 +18,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 // RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 	if app.UseCache {
 		// get the template cache from the app config
@@ -38,7 +40,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	_ = t.Execute(buf, td)
 
